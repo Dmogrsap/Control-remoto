3549433630a5f46ba49f1cc820d98fa223b0ce3e
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2021-02-24 21:14:51 -0700

    Add files via upload

---
+++ login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: "app-login",
+  templateUrl: ["./login.component.html"],
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent {
+  email: string;
+  password: string;
+
+  constructor() {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+  }
+}
