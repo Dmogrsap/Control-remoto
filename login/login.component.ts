feaca585d61f43029f2a39823da9e3b9a29b6744
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2021-02-24 22:55:04 -0700

    Update login.component.ts

--- login/login.component.ts
+++ login/login.component.ts
@@ -1,14 +1,29 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
+
+  
 })
 export class LoginComponent {
-  email: string | undifined;
-  password: string | undifined;
+
+  optionsForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    contraseña: new FormControl('', Validators.required),
+});
+
+submit() {
+    console.log(this.optionsForm.get('email')?.value)
+    console.log(this.optionsForm.get('contraseña')?.value)
+}
+  email: string | undefined;
+  password: string | undefined;
 
   constructor() {}
 
