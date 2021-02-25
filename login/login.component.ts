e1bb1127dc7b7b3f1f08d3052bb8e031afe86024
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2021-02-24 21:33:54 -0700

    Update login.component.ts

--- login/login.component.ts
+++ login/login.component.ts
@@ -3,12 +3,12 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-login",
-  templateUrl: ["./login.component.html"],
+  templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-  email: string;
-  password: string;
+  email: string | undifined;
+  password: string | undifined;
 
   constructor() {}
 
