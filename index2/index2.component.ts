d3fb77e5539d06df88b060087dbd9f24bcfe5088
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2021-02-25 21:43:39 -0700

    ts, css and html loading for website

---
+++ index2/index2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-index2',
+    templateUrl: './index2.component.html',
+    styleUrls: ['./styles.css']
+})
+
+export class index2Component{
+
+}
