7511f2b723174ecb757726c3ea17206508ae21ff
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2021-02-25 21:47:05 -0700

    Importación de index2 agregado y ruta del mismo

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -4,10 +4,12 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { productoComponent} from "./producto/producto.Component";
+import { index2Component } from './index2/index2.component';
 
 const routes: Routes = [
   
     { path: "", component: AppComponent, pathMatch: "full" },
+    { path: "index2", component: index2Component, pathMatch: "full" },
     { path: "login", component: LoginComponent, pathMatch: "full" },
     { path: "register", component: RegisterComponent, pathMatch: "full" },
     { path: "producto", component: productoComponent, pathMatch: "full" }
