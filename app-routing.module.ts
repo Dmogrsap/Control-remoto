e36795fbe38434b2d80f23a67de4a3f0c6a878bc
Dmogrsap <72819804+Dmogrsap@users.noreply.github.com>
2021-03-25 21:03:19 -0700

    Update app-routing.module.ts

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { productoComponent} from "./producto/producto.Component";
 import { index2Component } from './index2/index2.component';
+import { portadacomponent } from './portada/portada.component';
 
 const routes: Routes = [
   
